a1b3261ab6fe5c0d8ff7f5ef2b9390e0bd443556
Dheeraj Ahuja <dheeraj.ahuja@sap.com>
2022-08-22 13:52:13 +0530

    Expense item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,13 @@
-export default function ExpenseItem(){
-    return(<h1>ExpenseItem component</h1>)
+import "./ExpenseItem.css";
+
+export default function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 29, 2020</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price ">$2899.80</div>
+      </div>
+    </div>
+  );
 }
